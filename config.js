1c54b5aac9de4210f95fe022af30e97845268c1b
Andrurpk <cora.rpk@gmail.com>
2023-07-13 12:32:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const config = {
     links: [{ text: '', link: '' }],
     search: {
       enabled: false,
-      indexName: '',
+      indexName: 'dev_learn',
       algoliaAppId: process.env.GATSBY_ALGOLIA_APP_ID,
       algoliaSearchKey: process.env.GATSBY_ALGOLIA_SEARCH_KEY,
       algoliaAdminKey: process.env.ALGOLIA_ADMIN_KEY,
